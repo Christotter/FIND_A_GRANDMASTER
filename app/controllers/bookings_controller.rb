269e4e7ef50f88e6c4171efd1a6d1ab435bd5367
Lali Bibilashvili <lalibibilashvili@gmail.com>
2022-02-23 13:16:03 +0100

    created bookings, changed log-in field in navbar, corrected gm-new elo error

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,38 +1,39 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = Booking.all
   end
 
   def new
-   # @grandmaster = Grandmaster.find(params[:grandmaster_id])
-   # @user = User.find(params[:user_id])
-   # @booking = Booking.new
+    @grandmaster = Grandmaster.find(params[:grandmaster_id])
+    # @user = User.find(params[:user_id])
+    @booking = Booking.new
   end
 
   def create
-   # @grandmaster = Grandmaster.find(params[:grandmaster_id])
-   # @user = User.find(params[:user_id])
-   # @booking = Booking.new(booking_params)
-   # @booking.grandmaster = @grandmaster
-   # @booking.user = @user
-
-  #  if @bookmark.save
-  #    redirect_to @list, notice: 'Bookmark succesfully created'
-  #  else
-  #    render :new
-  #  end
+    @grandmaster = Grandmaster.find(params[:grandmaster_id])
+    # @user = User.find(params[:user_id])
+    @booking = Booking.new(booking_params)
+    @booking.grandmaster = @grandmaster
+    @booking.user = current_user
+    #@booking.user = @user
+    if @booking.save
+      redirect_to root_path, notice: 'Booking succesfully created'
+    else
+      render :new
+    end
   end
 
   def destroy
-   # @booking = Booking.find(params[:id])
-   # @list = @bookmark.list
-   # @booking.destroy
-   # redirect_to grandmaster_path(@grandmaster)
-  # redirect_to user_path(@user)
+    @booking = Booking.find(params[:id])
+    # @list = @bookmark.list
+    @booking.destroy
+    redirect_to grandmaster_path(@grandmaster)
+    # redirect_to user_path(@user)
   end
 
   private
 
   def booking_params
-   # params.require(:booking).permit(:message, :status, :user_id, :gm_profile_id)
+    params.require(:booking).permit(:message)
   end
 end
