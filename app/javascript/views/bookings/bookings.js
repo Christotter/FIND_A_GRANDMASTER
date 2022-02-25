830df0ba021d413cd3782bdf0526604669c1bd38
Christopher Johnson <christo@web-ch.net>
2022-02-25 11:42:31 +0100

    javascript error fixed

--- app/javascript/views/bookings/bookings.js
+++ app/javascript/views/bookings/bookings.js
@@ -2,25 +2,29 @@
 
 const handleBookingStatus = () => {
   // console.log("Calling bookings js")
-  let checkedValue = document.getElementById('choice');
-  let pupil = document.getElementById("pupil");
-  let grandmaster = document.getElementById("grandmaster");
 
-  window.addEventListener("load", function(event) {
+  window.addEventListener("turbolinks:load", function(event) {
+    let checkedValue = document.getElementById('choice');
+    let pupil = document.getElementById("pupil");
+    let grandmaster = document.getElementById("grandmaster");
+
+    if (!checkedValue) { return; }
+
     grandmaster.style.display = "none";
     pupil.style.display = "block";
+
+    checkedValue.addEventListener("change", (e) => {
+      // console.log(checkedValue.checked);
+      if (checkedValue.checked === false) {
+        grandmaster.style.display = "none";
+        pupil.style.display = "block";
+      } else {
+        // console.log(pupil);
+        pupil.style.display = "none";
+        grandmaster.style.display = "block";
+      }
   });
 
-  checkedValue.addEventListener("change", (e) => {
-    // console.log(checkedValue.checked);
-    if (checkedValue.checked === false) {
-      grandmaster.style.display = "none";
-      pupil.style.display = "block";
-    } else {
-      // console.log(pupil);
-      pupil.style.display = "none";
-      grandmaster.style.display = "block";
-    }
   })
 }
 
